c7bb15859e4a51ddae81972c0405045d352885d2
Kamden-05 <kamdenw05@gmail.com>
2025-10-27 14:51:39 -0500

    created pitstop calss

--- src/models/pitstop.ts
+++ src/models/pitstop.ts
@@ -1,15 +1,60 @@
-export interface PitStop {
+export interface PitStopDto {
   prev_stint_id: number;
 
   // start pit stop attributes
   road_enter_time: number;
   service_start_time: number;
-  required_repair_time: number;
-  optional_repair_time: number;
   refuel_amount: number;
+  repairs: boolean;
   tire_change: boolean;
 
   // end pit stop attributes
   service_end_time: number;
   road_exit_time: number;
+}
+
+export class PitStop {
+  constructor(readonly prevStintId: number, readonly roadEnterTime: number) {}
+
+  serviceStartTime?: number;
+  requiredRepairTime?: number;
+  optionalRepairTime?: number;
+  refuelAmount?: number;
+  serviceEndTime?: number;
+  roadExitTime?: number;
+
+  // tires
+  leftFront?: boolean;
+  rightFront?: boolean;
+  leftRear?: boolean;
+  rightRear?: boolean;
+
+  get hasRepairs(): boolean {
+    return ((this.optionalRepairTime ?? 0) + (this.requiredRepairTime ?? 0)) > 0;
+  }
+
+  get hasTireChange(): boolean {
+    return [this.leftFront, this.rightFront, this.leftRear, this.rightRear].some(t => t === true);
+  }
+
+  get pitDuration(): number | undefined{
+    return this.roadExitTime === undefined ? undefined : this.roadExitTime - this.roadEnterTime;
+  }
+
+  get boxTime(): number | undefined {
+    return this.serviceEndTime === undefined ? undefined : this.serviceEndTime - this.serviceStartTime;
+  }
+
+  toDto(): PitStopDto {
+    return {
+      prev_stint_id: this.prevStintId,
+      road_enter_time: this.roadEnterTime,
+      service_start_time: this.serviceStartTime,
+      refuel_amount: this.refuelAmount,
+      repairs: this.hasRepairs,
+      tire_change: this.hasTireChange,
+      service_end_time: this.serviceEndTime,
+      road_exit_time: this.roadExitTime,
+    };
+  }
 }
