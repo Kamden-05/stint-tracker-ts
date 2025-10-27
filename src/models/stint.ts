c8f58f90acf867d6a644ffd3e8329c71cd2b3a3f
Kamden-05 <kamdenw05@gmail.com>
2025-10-27 15:05:14 -0500

    created stint class

--- src/models/stint.ts
+++ src/models/stint.ts
@@ -1,5 +1,5 @@
-export interface Stint {
-  driver: string;
+export interface StintDto {
+  driver_name: string;
   number: number;
 
   // start attributes
@@ -13,4 +13,44 @@ export interface Stint {
   end_position?: number;
   end_fuel?: number;
   end_incidents?: number;
+}
+
+export class Stint {
+  laps: number[];
+
+  constructor(
+    readonly driverName: string,
+    readonly stintNumber: number,
+    readonly startTime: number,
+    readonly startPosition: number,
+    readonly startFuel: number,
+    readonly startIncidents: number,
+  ) {
+    this.laps = [];
+  }
+ 
+  stintId?: number;
+  endTime?: number;
+  endPosition?: number;
+  endFuel?: number;
+  endIncidents?: number;
+
+  toDto(): StintDto{
+    return {
+      driver_name: this.driverName,
+      number: this.stintNumber,
+      
+      // start values
+      start_time: this.startTime,
+      start_position: this.startPosition,
+      start_fuel: this.startFuel,
+      start_incidents: this.startIncidents,
+
+      // end values
+      end_time: this.endTime,
+      end_position: this.endPosition,
+      end_fuel: this.endFuel,
+      end_incidents: this.endIncidents,
+    }
+  }
 }
